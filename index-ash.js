406f14403f5e4ffab7f734589bd93fa463e6cde9
Ashley Follen <ashleyfollen@outlook.com>
2021-08-04 14:20:54 -0600

    started event listeners

--- index-ash.js
+++ index-ash.js
@@ -1,15 +1,41 @@
 const url = "http://localhost:3000/albums"
-const divContainer = document.getElementById('albums-container')
+const divContainer = document.getElementById('albums-container');
+const divGenreFilter = document.getElementsByClassName('filter-genre');
+const rockDiv = document.getElementById('rock');
+const wishlistHeart = document.querySelectorAll("#like-glyph");
+
+const glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+  };
+  
+const colorStates = {
+    "red" : "",
+    "": "red"
+  };
+
 
 fetch(url)
 .then(resp => resp.json())
 .then(handleAlbums) 
 
+//let rockFilter = '';
+
+rockDiv.addEventListener('click', (e) => {
+    const rockFilter = e.target.textContent;
+    console.log(rockFilter);
+    //option to switch this to e.target.id
+});
+
+
 function handleAlbums(albums){
+    //if album.genre-filter === genreFilter,
     albums.forEach(appendElements)
 }
 
 function appendElements(album){
+    
+    //if album.genre-filter === genreFilter, then do all this below: 
     const albumDiv = document.createElement('div')
     const trackDiv = document.createElement('div')
 
@@ -19,6 +45,7 @@ function appendElements(album){
     const genrePg = document.createElement('p')
     const yearPg = document.createElement('p')
     const tracksHeader = document.createElement('h4')
+    const wishlist = document.createElement('div')
 
     imgTag.className = 'album-cover'
     //albumDiv.className = 'album-div'
@@ -30,17 +57,26 @@ function appendElements(album){
     imgTag.src = album.artwork
     nameHeader.textContent = album.name 
     artistHeader.textContent = album.artist 
-    genrePg.innerText = `Genre: ${album.genre}`
-    yearPg.innerText = `Year: ${album.year}`
+    genrePg.innerText = album.genre
+    yearPg.innerText = album.year
     tracksHeader.innerText = "Tracklist"
+    wishlist.innerHTML = `<li class="wishlist">Wishlist <span class="like-glyph">&#x2661;</span></li>`
+    
 
     divContainer.append(albumDiv)
-    albumDiv.append(imgTag, nameHeader, artistHeader, genrePg, yearPg, trackDiv)
+    albumDiv.append(imgTag, nameHeader, artistHeader, genrePg, yearPg, trackDiv, wishlist)
     trackDiv.append(tracksHeader)
+
+    wishlist.addEventListener('click', e => {
+        const heart = e.target;
+        heart.innerText = glyphStates[heart.innerText];
+        heart.style.color = colorStates[heart.style.color];
+    });
 
     tracks.forEach((tracks) => {
         const trackLi = document.createElement('li')
         trackLi.innerText = tracks
         trackDiv.append(trackLi)
-    })
+        });
 }
+
